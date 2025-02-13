9807a21fd21d5b362eb805551e3d3430c6947525
Bit D13 <bit@d13.co>
2025-02-13 01:30:34 +0100

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
+set -e
+
+cd "$(dirname "$(realpath "$0")")"
+
+echo rimraff public
+echo ==============
+
+rm -rf public
+
+echo build index
+echo ===========
+
+cd index
+npm run build
+cd ..
+
+mv index/dist public
+
+echo build dualSTAKE
+echo ===============
+
 mdbook build dualstake --dest-dir ../public/dualSTAKE/
+
